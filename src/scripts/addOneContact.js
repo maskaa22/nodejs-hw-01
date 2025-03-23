540815d6017a8212fb34fc4315b9ddac477436a1
Mariia Skydan <kyrosika565@gmail.com>
2025-03-23 09:31:40 +0200

    all code

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const contactlist = await readContacts();
+  const newContact = createFakeContact();
+  await writeContacts([...contactlist, newContact]);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactlist = await readContacts();
+  const newContact = Array(number).fill(0).map(createFakeContact);
+  await writeContacts([...contactlist, ...newContact]);
+};
 
 generateContacts(5);
